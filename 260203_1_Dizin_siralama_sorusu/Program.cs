08325893d472095cb0517929a7a9d19635073707
resulturfent <rsltrfnt@gmail.com>
2026-02-03 21:29:24 +0300

    ArrayList, DiziSıarala örnek

---
+++ 260203_1_Dizin_siralama_sorusu/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace _260203_1_Dizin_siralama_sorusu;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        int elemanSayisi = 5;
+        int[] dizi = new int[elemanSayisi];
+        //int[] dizi = { 11, 3, 9, 5, 2 };
+
+        for (int i = 0; i < dizi.Length; i++)
+        {
+            Console.WriteLine(i + 1 + ". sayıyı giriniz:");
+            dizi[i] = Convert.ToInt32(Console.ReadLine());
+        }
+        //Dizi=>11,3,9,5,2
+        for (int i = 0; i < dizi.Count(); i++)
+        {
+            //dizi[i]=>11
+
+            for (int k = 0; k < dizi.Count(); k++)
+            {
+                int gecici;
+                if (dizi[k] > dizi[i])
+                {
+                    gecici = dizi[k];
+                    dizi[k] = dizi[i];
+                    dizi[i] = gecici;
+                }
+
+            }
+        }
+
+
+        for (int i = 0; i < dizi.Length; i++)
+        {
+            Console.Write(dizi[i]+"-");
+        }
+    }
+}
---
+++ 260203_2_collection/Program.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;//ArrayList için gerekli
+
+namespace _260203_2_collection;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+
+        //List,Collection,Array, Delegate
+        //ArrayList kullanmak için using System.Collections; kütüphanesini en başa eklemeniz gereklidir
+        ArrayList arrayList = new ArrayList();
+        
+        arrayList.Add("Arı Bilgi");//ArrayList için eleman Add ile eklenir
+        arrayList.Add(45);
+        arrayList.Add(25.5m);//decimal
+        arrayList.Add(true);
+        arrayList.Add(DateTime.Now);
+
+        for (int i = 0; i < arrayList.Count; i++)
+        {
+            Console.WriteLine(arrayList[i]);
+        }
+
+        ArrayList sayilar = new ArrayList();
+        sayilar.Add(87);
+        sayilar.Add(23);
+        sayilar.Add(54);
+        sayilar.Add(5);
+        sayilar.Add(4);
+        sayilar.Add(1);
+
+        sayilar.Sort();//küçükten büyüğe doğru sıralama yapar
+
+        for (int i = 0; i < sayilar.Count; i++)
+        {
+            Console.Write(sayilar[i] +"-");
+        }
+        Console.WriteLine();
+        int kapasite = arrayList.Capacity;//kapasite 2 nin üstü şeklinde değer ile artar
+        //
+        Console.WriteLine("Kapasite, eleman sayısı değildir:"+kapasite);
+        Console.WriteLine("count ile eleman sayısı:"+arrayList.Count);//Count , ArrayList eleman sayısını verir
+                                                                      //index nedir??
+        Console.WriteLine("index no:" +arrayList.IndexOf("Arı Bilgi"));
+        Console.WriteLine("index no:" +arrayList.IndexOf(25.5m));
+        Console.WriteLine("45 elemanı var mı:"+arrayList.Contains(45));
+
+        if (arrayList.Contains(45))
+        {
+            Console.WriteLine( "Index numarası:"+arrayList.IndexOf(45));
+        }
+        //*************************************************************
+        //ArrayList kopyalamak=> Devam edilecek?
+
+
+  
+    }
+}
