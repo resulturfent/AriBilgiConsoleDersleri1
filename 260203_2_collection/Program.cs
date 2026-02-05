1b2b9d154c648feaec5d0fc0beedb9e80af55c0e
resulturfent <rsltrfnt@gmail.com>
2026-02-05 20:00:28 +0300

    ArrayList için kopyalama yöntemleri

--- 260203_2_collection/Program.cs
+++ 260203_2_collection/Program.cs
@@ -10,18 +10,19 @@ internal class Program
         //List,Collection,Array, Delegate
         //ArrayList kullanmak için using System.Collections; kütüphanesini en başa eklemeniz gereklidir
         ArrayList arrayList = new ArrayList();
-        
+
         arrayList.Add("Arı Bilgi");//ArrayList için eleman Add ile eklenir
         arrayList.Add(45);
         arrayList.Add(25.5m);//decimal
         arrayList.Add(true);
         arrayList.Add(DateTime.Now);
 
+        Console.WriteLine("------array List elemanları-------------");
         for (int i = 0; i < arrayList.Count; i++)
         {
             Console.WriteLine(arrayList[i]);
         }
-
+        Console.WriteLine("------eleman SON----");
         ArrayList sayilar = new ArrayList();
         sayilar.Add(87);
         sayilar.Add(23);
@@ -31,29 +32,86 @@ internal class Program
         sayilar.Add(1);
 
         sayilar.Sort();//küçükten büyüğe doğru sıralama yapar
-
         for (int i = 0; i < sayilar.Count; i++)
         {
-            Console.Write(sayilar[i] +"-");
+            Console.Write(sayilar[i] + "-");
         }
         Console.WriteLine();
         int kapasite = arrayList.Capacity;//kapasite 2 nin üstü şeklinde değer ile artar
         //
-        Console.WriteLine("Kapasite, eleman sayısı değildir:"+kapasite);
-        Console.WriteLine("count ile eleman sayısı:"+arrayList.Count);//Count , ArrayList eleman sayısını verir
-                                                                      //index nedir??
-        Console.WriteLine("index no:" +arrayList.IndexOf("Arı Bilgi"));
-        Console.WriteLine("index no:" +arrayList.IndexOf(25.5m));
-        Console.WriteLine("45 elemanı var mı:"+arrayList.Contains(45));
+        Console.WriteLine("Kapasite, eleman sayısı değildir:" + kapasite);
+        Console.WriteLine("count ile eleman sayısı:" + arrayList.Count);//Count , ArrayList eleman sayısını verir
+                                                                        //index nedir??
+        Console.WriteLine("index no:" + arrayList.IndexOf("Arı Bilgi"));
+        Console.WriteLine("index no:" + arrayList.IndexOf(25.5m));
+        Console.WriteLine("45 elemanı var mı:" + arrayList.Contains(45));
 
         if (arrayList.Contains(45))
         {
-            Console.WriteLine( "Index numarası:"+arrayList.IndexOf(45));
+            Console.WriteLine("Index numarası:" + arrayList.IndexOf(45));
         }
+        Console.WriteLine("------Array List Kopya devam-------------");
+
         //*************************************************************
         //ArrayList kopyalamak=> Devam edilecek?
 
+        ArrayList newArrayList = new ArrayList(arrayList);//arrayList elemanlarını newArrayList içine kopyalar/atar
 
-  
+        for (int i = 0; i < newArrayList.Count; i++)
+        {
+            Console.WriteLine(newArrayList[i]);
+        }
+        Console.WriteLine("-----Yeni Kopya ArrayList");
+        arrayList.Add("Clone işlemi");
+        ArrayList newCopyList = (ArrayList)arrayList.Clone();//ArrayList bu işlemle newCopyList e kopyalanır.(ArrayList) işlemi class düzeyinde bir cast(tip dönüşümü) işlemidir.
+
+        for (int i = 0; i < newCopyList.Count; i++)
+        {
+            Console.WriteLine(newCopyList[i]);
+        }
+        //**********************************************
+        Console.WriteLine("------Döngü ile ekleme----------");
+        ArrayList arrayListDongu = new ArrayList();
+
+        for (int i = 0; i < arrayList.Count; i++)
+        {
+            Console.WriteLine(i + 1 + ".eleman type:" + arrayList[i].GetType());
+            arrayListDongu.Add(arrayList[i]);
+            Console.WriteLine(arrayListDongu[i]);
+        }
+
+        Console.WriteLine("-------------Sayılar ArrayList-------");
+        sayilar.Reverse();//elemanları ındex numarasına göre ters çevirir
+        for (int i = 0; i < sayilar.Count; i++)
+        {
+            Console.WriteLine(sayilar[i]);
+        }
+        //ArrayList için CopyTo işlemi
+        Console.WriteLine("-----------CopyTo ile işlem------------");
+        string[] sehirler = new string[12];
+        sehirler[0] = "İzmir";
+        sehirler[1] = "Mardin";
+        sehirler[2] = "Zonguldak";
+        sehirler[3] = "Muğla";
+        sehirler[4] = "Diyarbakır";
+        sehirler[5] = "Iğdır";
+
+
+        ArrayList city = new ArrayList();
+        city.Add("Adana");
+        city.Add("Isparta");
+        city.Add("Kars");
+        city.Add("Ankara");
+        city.Add("Malatya");
+        city.Add("Mersin");
+
+        //city.CopyTo(cityIndex,kopyalanacak Colleciton/Dizi,üzerine yazılacak Index,başlangıç değeri);
+        city.CopyTo(1, sehirler, 9, 1);//1 index City de Isparta, sehirler 9. ındex e taşı
+        city.CopyTo(4, sehirler, 11, 1);//4 index City de Malatya, sehirler 11. index e taşı
+
+        for (int i = 0; i < sehirler.Count(); i++)
+        {
+            Console.WriteLine(i+".index:"+ sehirler[i]);
+        }
     }
 }
