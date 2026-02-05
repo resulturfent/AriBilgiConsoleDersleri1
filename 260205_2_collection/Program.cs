60f361ff2b7c1126a80109901e13539ee7d378f6
resulturfent <rsltrfnt@gmail.com>
2026-02-05 21:28:20 +0300

    soru ve ödev

---
+++ 260205_2_collection/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.ObjectModel;
+
+namespace _260205_2_collection;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        //Collection kullanmak için using System.Collections.ObjectModel; kütüphanesini en üst taraf olan using alanına eklenmesi gereklidir
+        Collection<string> list1 = new Collection<string>();//Çalışanların isimleri
+        list1.Add("C#");
+
+
+        List<int> sayilar = new List<int>();//Fabrika çalışan sırasını
+        sayilar.Add(10);
+
+        Collection<double> maaslar = new Collection<double>();//Maaşları  listelemek için kullanılabilir
+        maaslar.Add(65125);
+        maaslar.Add(35125);
+        maaslar.Add(65100);
+        maaslar.Add(95450);
+        //foreach=> eleman sayısı belli olmayan Collection yapılar için listeleme yapar
+        foreach (var item in maaslar)
+        {
+            Console.WriteLine(item);
+        }
+
+
+    }
+}
+
---
+++ 260205_3_collection_ornek1/Program.cs
@@ -0,0 +1,67 @@
+﻿
+using System.Collections;
+using System.Collections.ObjectModel;
+
+namespace _260205_3_collection_ornek1;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        //kullanıcıdan alınan adı,soyadı, doğum tarihi, cinsiyet, medeni durum, aylık gelir gibi işlemleri bir ArrayList içinde kullanarak kullanıcıya bir kart oluşturup kullanıcıya gösteriniz
+        /*
+         AD:
+        SOYADI:
+        DOĞUM TARİHİ:
+        CİNSİYET:
+        MEDENİ DURUM:
+        AYLIK GELİR:
+         
+         
+         */
+
+        string[] kart = { "AD", "SOYAD", "DOĞUM TARİHİ", "CİNSİYET", "MEDENİ DURUM", "AYLIK GELİR" };
+
+        Collection<string> list = new Collection<string>();
+
+        for (int i = 0; i < 3; i++)
+        {
+            ArrayList kartItem = new ArrayList();
+            Console.WriteLine(i + 1 + ".Kişi Bilgileri");
+            foreach (var item in kart)
+            {
+                if (item == "MEDENİ DURUM")
+                {
+                    Console.WriteLine("Bekar 1,evli için 0 yazınız");
+                }
+                else
+                {
+                    Console.WriteLine(item + " giriniz?");
+                }
+
+                kartItem.Add(Console.ReadLine());
+            }
+        }
+
+        //for (int i = 0; i < kartItem.Count; i++)
+        //{
+        //    if (kart[i] == "MEDENİ DURUM")
+        //    {
+        //        if (kartItem[i] == "1")
+        //        {
+        //            Console.WriteLine(kart[i] + ":Bekar");
+        //        }
+        //        else
+        //        {
+        //            Console.WriteLine(kart[i] + ":Evli");
+        //        }
+        //    }
+        //    else
+        //    {
+        //    Console.WriteLine(kart[i] + ":" + kartItem[i]);
+        //    }
+        //}
+        //bu soru eğer 5 den fazla kişi kartı istenseydi nasıl bir yol izlenirdi, çözünüz?
+
+    }
+}
