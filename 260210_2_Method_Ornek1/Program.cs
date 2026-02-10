fe4c8a61523a9dfca9f0f826ff0171e98c928fff
resulturfent <rsltrfnt@gmail.com>
2026-02-10 21:28:30 +0300

    method örnekler

---
+++ 260210_2_Method_Ornek1/Program.cs
@@ -0,0 +1,51 @@
+﻿namespace _260210_2_Method_Ornek1;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        //Kullanıcıdan alınan 3 sayının toplamını veren işlemi method kullanarak kodlayıınız
+
+        int toplam = 0;
+
+        for (int i = 0; i < 3; i++)
+        {
+            Console.WriteLine(i + 1 + ".sayi:");
+            int sayi = Convert.ToInt32(Console.ReadLine());
+            toplam = toplam + sayi;
+        }
+        Console.WriteLine("3 sayının toplamı: " + toplam);
+        //---------------------------------------------------------------------------------
+        //Method ile çözüm
+        //SOLID Prensipler=> Dünya çapında yazılım kodlamak için uyulan kuralları içeren bir kısa isimdir
+        //Min SOLID için 2 makale okunacak
+
+        int s1 = SayiAl();
+        toplam = Islem(s1, toplam);
+
+        int s2 = SayiAl();
+        toplam = Islem(s2, toplam);
+
+        int s3 = SayiAl();
+        toplam = Islem(s3, toplam);
+
+        EkranaYaz("3 sayının toplamı: " + toplam);
+
+    }
+    static int SayiAl()
+    {
+        //Console.WriteLine(".sayi giriniz?");
+        EkranaYaz("Sayı giriniz?");
+        int sayi = Convert.ToInt32(Console.ReadLine());
+        return sayi;
+    }
+
+    static int Islem(int sayi1, int toplam)
+    {
+        return toplam + sayi1;
+    }
+    static void EkranaYaz(string metin)
+    {
+        Console.WriteLine(metin);
+    }
+}
---
+++ 260210_3_Method_Ornek2/Program.cs
@@ -0,0 +1,93 @@
+﻿namespace _260210_3_Method_Ornek2;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        //Kullanıcıdan alınan 3 sayıyı küçükten büyüğe doğru sıralamasını yapan işlemi kodlayınız
+
+        int s1 = SayiAl();
+        int s2 = SayiAl();
+        int s3 = SayiAl();
+        //s1 en büyük ise x=s1
+        Siralama(s1, s2, s3);
+        //s2 en büyük ise x=s2
+        Siralama(s2, s1, s3);
+        //s3 en büyük ise x=s3
+        Siralama(s3, s1, s2);//
+        //2 tane method sorusu paylaşılacak
+        #region My Code but not short
+        /*
+        * if (s1>s2 && s1>s3)//s1 en büyük
+        {
+            if (s2>s3)
+            {
+                Console.WriteLine(s1+">"+s2+">"+s3);
+            }
+            else if (s3>s2)
+            {
+                Console.WriteLine(s1 + ">" + s3 + ">" + s2);
+            }
+            else
+            {
+                Console.WriteLine(s1 + ">" + s3 + "=" + s2);
+
+            }
+        }
+        else if (s2 > s1 && s2 > s3)//s2 en büyük
+        {
+            if (s1 > s3)
+            {
+                Console.WriteLine(s2 + ">" + s1 + ">" + s3);
+            }
+            else if (s3 > s1)
+            {
+                Console.WriteLine(s2 + ">" + s3 + ">" + s1);
+            }
+            else
+            {
+                Console.WriteLine(s2+ ">" + s3 + "=" + s1);
+
+            }
+        }//...
+       */
+        #endregion
+
+
+    }
+
+    /// <summary>
+    /// int sayıyı kullanıcıdan alır
+    /// </summary>
+    /// <returns></returns>
+    static int SayiAl()
+    {
+        Console.WriteLine("Sayı giriniz?");
+        return Convert.ToInt32(Console.ReadLine());
+    }
+
+    /// <summary>
+    /// 3 sayı arasında sıralama yapar
+    /// </summary>
+    /// <param name="x"></param>
+    /// <param name="y"></param>
+    /// <param name="z"></param>
+    static void Siralama(int x, int y, int z)
+    {
+        if (x > y && x > z)//x en büyük
+        {
+            if (y > z)
+            {
+                Console.WriteLine(x + ">" + y + ">" + z);
+            }
+            else if (z > y)
+            {
+                Console.WriteLine(x + ">" + z + ">" + y);
+            }
+            else
+            {
+                Console.WriteLine(x + ">" + z + "=" + y);
+            }
+        }       
+    }
+}
