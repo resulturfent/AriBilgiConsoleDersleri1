5b9c7d87c493f830bf6484de90a47d9d85af298c
resulturfent <rsltrfnt@gmail.com>
2026-02-17 21:30:12 +0300

    top, orer by, operatörler eklendi

--- SQLDersleri/260217_1_Giriş Alias, where, order by,top,Operatörler.sql
+++ SQLDersleri/260217_1_Giriş Alias, where, order by,top,Operatörler.sql
@@ -43,9 +43,42 @@ select
  --Sipariþler(Order) tablosunda Sipariþ tarihi(OrderDate), Kargo maliyeti(Freight),Nakliye þehri,ülkesi(shipCity,ShipCountry)þeklinde listeleyiniz
  select OrderDate as [Sipariþ Tarihi],Freight 'Kargo Maliyeti', ShipCity 'Nakliye Þehri',ShipCountry 'Nakliye ülkesi'  from Orders
  ------------------------------------------------
- --MS-SQL=> Microsoft -Stracture(Mimari, yapý) Query(Sorgu) Language (Dil)
+ --MS-SQL=> Microsoft -Structured(Mimari, yapý) Query(Sorgu) Language (Dil)
  --SQL bir programlama dili deðildir
  --T-SQL=> Transact SQL
+ --where, order by, top, operatörler, hazýr fonksiyonlar, group by, having, koþul, döngü, deðiþken tanýmý(declare),fonksiyon, view, stored procedure,trigger,create,alter,drop,insert, update, delete, .. iþlemleri yapar 
+ --where
+ select *from Products where unitprice=10
+ select ProductName,UnitPrice,UnitsInStock,QuantityPerUnit from Products where unitprice=10
+ /*
+ Operatörler
+ <,>,<=,>=,!=,=,+,-,*,/,%=> c# ta olduðu gibi kullanýlýr
+ and,or=> and (&&)ve or(||) c# taki yapýlar ile ayný þekilde kullanýlýr 
+ is nul,is not null=>??
+ in, between =>??
+ */
+ select *from Orders where ShipCountry<>'Brazil'--<> eþit deðildir
+ select *from Orders where ShipCountry!='Brazil'--!= eþit deðildir
+ select *from Products where UnitPrice<>10 and UnitsInStock>100--fiyatý 10 olmayan ve stoðu 100 den büyük olan datalarý listeledik
+ --sayý sorgularý direk yazýlýr ama metin için '' (tek týrnak) alýnmasý gereklidir
+ --sýralama için order by kulanýlýr
+ --order=> sipariþ, sýralama
+ select *from customers order by City-- a dan z ye ya da küçükten büyüðe doðru sýralar. Kolon adýndan sonra bir þey yazýlmazsa oto olarak asc(ascanding) kabul eder 
+ select *from customers order by City asc
+ select *from customers order by City desc--descanding=> z'den a'ya ya da büyükten küçüðe doðru sýralama yapar
+ --Order by her zaman bütün scriptin(SQL kod parçacýðý) sonunda yazýlýr
+ --Order by ile beraber genelde top kullanýlýr
+ select top 15 *from customers order by City asc--þehirlere göre a'dan z'ye sýrlayarak ilk 15 datayý listeledik
+ --aynýsýný aþaðýda kolon ismi vererek yapalým
+  select 
+  top 15 CustomerID as Kod,
+  CompanyName		as [Firma Adý],
+  ContactName		as 'Yetkili Kiþi',
+  City				as Þehir,
+  Country			as Ülke 
+  from customers order by City 
+
+
 
 
 
