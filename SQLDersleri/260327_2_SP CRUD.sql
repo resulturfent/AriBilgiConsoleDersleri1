35f00abe9af300087d80d6a6309069ed0981cc16
resulturfent <rsltrfnt@gmail.com>
2026-03-27 20:34:44 +0300

    sql sp,

---
+++ SQLDersleri/260327_2_SP CRUD.sql
@@ -0,0 +1,6 @@
+---*******************************
+--27 Mart 2026 SQL Dersleri
+--****************************************
+--SP ile CRUD iþlemleri
+--SP ile Insert, Update, Delete iþlemlerinin yapýlmasý
+
